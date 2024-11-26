1ba037e313e1a8f004ee99d3213c8f78da1af9c6
spongeboi <hi@spongeboi.com>
2024-11-25 17:56:03 -0800

    fix

--- components/MealList.tsx
+++ components/MealList.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Meal } from '../../services/apiService'
+import { Meal } from '@/services/apiService'
 
 interface Props {
   meals: Meal[];
